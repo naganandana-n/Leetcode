5c29856f3c5afb9c82a7da6ec4384f0890a3102a
naganandana <naganandana17@gmail.com>
2024-06-17 10:56:47 +0400

    hashmap

---
+++ LC 49.Group Anagrams.py
@@ -0,0 +1,34 @@
+'''
+49. Group Anagrams
+
+https://leetcode.com/problems/group-anagrams/description/
+
+'''
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # using hashmap(dict)
+        # what im doing is, the key will be letters in the word
+        # and the value is the list of words that match that
+
+        # IMPORTANT FUNCTION:
+        # ORD( ) -> gets ascii value of letter
+
+        result = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26 # a...z
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            result[tuple(count)].append(s) # dicts can't take lists as key
+        
+        answer = []
+        for key, value in result.items():
+            local = []
+            for i in value:
+                local.append(i)
+            answer.append(local)
+        return answer
+        
+        
