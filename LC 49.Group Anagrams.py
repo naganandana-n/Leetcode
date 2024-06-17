fa03c74d9d4292f286457bb00bb8d164f20d1611
naganandana <naganandana17@gmail.com>
2024-06-17 11:02:31 +0400

    hashmap

--- LC 49.Group Anagrams.py
+++ LC 49.Group Anagrams.py
@@ -31,4 +31,20 @@ class Solution:
             answer.append(local)
         return answer
         
-        
+# Better solution (ONLY LAST PART OF SHOWING RESULT IS DIFFERENT - ONE LINE)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) #mapping character count to list of Anagrams
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            res[tuple(count)].append(s)
+
+        return res.values()
+
+        # O(m * n) m -> no. of strings, n -> avg length of a string
