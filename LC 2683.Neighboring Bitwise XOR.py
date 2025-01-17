5c9a3cac764d2304b8eddcb8659219fa66705e9a
naganandana <naganandana17@gmail.com>
2025-01-17 23:36:41 +0400

    array, bit manipulation

---
+++ LC 2683.Neighboring Bitwise XOR.py
@@ -0,0 +1,13 @@
+'''
+2683. Neighboring Bitwise XOR
+'''
+
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        ans = [0] * len(derived)
+        for i, j in enumerate(ans):
+            if i == len(dervied) - 1:
+                if j 
+            if j ^ ans[i+1] == derived[i]:
+                continue
+            elif 
